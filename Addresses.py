e1e30b3cffb5509ae9e7ef682622176f424e1098
Jack Kingsman <jack.kingsman@gmail.com>
2015-12-24 11:58:57 -0800

    faster email generation; actually closes #15

--- Addresses.py
+++ Addresses.py
@@ -1,24 +1,18 @@
-import random
+from random import choice, randint
+from itertools import product
 from Config import domain
 
 class CustomAddress():
+    def __init__(self):
+        self.vowels = 'aeiou'
+        self.consonants = 'bcdfgjklmnprstvz' # no hqx for pronouncability
+        self.choices = map(lambda x: "".join(x), product(self.consonants, self.vowels))
+        self.letterCount = 8
+        self.numberCount = 2
+
     def get(self):
-        letterCount = 8
-        numberCount = 2
-        vowels = 'aeiou'
-        consonants = 'bcdfgjklmnprstvz' # no hqx for pronouncability
-        randString = ''
+        randString = "".join(choice(self.choices) for _ in xrange(self.letterCount / 2))[:self.letterCount] + \
+                       str(randint(10**(self.numberCount-1), (10**self.numberCount)-1))
 
-        for i in range(letterCount):
-            if len(randString) == 0:
-                randString += random.choice(consonants)
-            else:
-                if randString[-1:] in consonants:
-                    randString += random.choice(vowels)
-                else:
-                    randString += random.choice(consonants)
-
-        randString += str(random.randint(10**(numberCount-1), (10**numberCount)-1))
         randEmail = randString + '@' + domain
-
         return randEmail
